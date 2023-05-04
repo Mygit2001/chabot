1d57226f71586c4ad5404d2ce9d40d1de8a01e85
Syle2001ht <77475693+Mygit2001@users.noreply.github.com>
2023-05-04 21:24:18 +0700

    update

--- src/controllers/chatbotController.js
+++ src/controllers/chatbotController.js
@@ -1,9 +1,11 @@
-require("dotenv").config()
+require("dotenv").config();
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
+
 let getHomepage = (req, res) => {
   return res.send("Xin chao");
 };
 let getWebhook = (req, res) => {
-    let VERIFY_TOKEN = process.env.VERIFY_TOKEN
   // Parse the query params
   let mode = req.query["hub.mode"];
   let token = req.query["hub.verify_token"];
@@ -24,16 +26,30 @@ let getWebhook = (req, res) => {
 };
 let postWebhook = (req, res) => {
   let body = req.body;
-  if(body.object === 'page') {
-    body.entry.forEach(entry => {
-        let webhook_event = entry.messaging[0];
-        console.log(webhook_event)
-    })
-    res.status(200).send('EVENT_RECEIVED');
-  }else {
-    res.sendStatus(404)
+  if (body.object === "page") {
+    body.entry.forEach((entry) => {
+      // Gets the body of the webhook event
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+
+      // Get the sender PSID
+      let sender_psid = webhook_event.sender.id;
+      console.log("Sender PSID: " + sender_psid);
+    });
+    res.status(200).send("EVENT_RECEIVED");
+  } else {
+    res.sendStatus(404);
   }
 };
+
+// Handles messages events
+function handleMessage(sender_psid, received_message) {}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {}
 module.exports = {
   getHomepage: getHomepage,
   getWebhook: getWebhook,
